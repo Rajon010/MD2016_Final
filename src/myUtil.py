b4383b1a3b3ae8283935ddf2990be9bb9d841718
Kevin <b03902086@ntu.edu.tw>
2017-01-14 22:34:11 +0800

    use itertools

--- src/myUtil.py
+++ src/myUtil.py
@@ -1,4 +1,5 @@
 from math import log
+import itertools
 
 def addCount2Dict(d, key, value):
 	if key in d:
@@ -10,30 +11,34 @@ def addCount2Dict(d, key, value):
 TUPLE = True
 COMBINATION = False
 
-def generateAllTupleOrCombinationRecursion(allPossibility, length, tupleElementUpperBound, isTupleOrCombination, tupleOrCombination):
-	if len(tupleOrCombination) == length:
-		allPossibility.append(tupleOrCombination)
-		return
-
-	if isTupleOrCombination:
-		# tuple
-		elementLowerBound = 0
-		elementUpperBound = tupleElementUpperBound
-	else:
-		# combination
-		if len(tupleOrCombination) == 0:
-			elementLowerBound = 0
-		else:
-			elementLowerBound = tupleOrCombination[len(tupleOrCombination) - 1] + 1
-		elementUpperBound = tupleElementUpperBound - (length - len(tupleOrCombination)) + 1
-		
-	for i in range(elementLowerBound, elementUpperBound):
-		generateAllTupleOrCombinationRecursion(allPossibility, length, tupleElementUpperBound, isTupleOrCombination, tupleOrCombination + (i,))
+# def generateAllTupleOrCombinationRecursion(allPossibility, length, tupleElementUpperBound, isTupleOrCombination, tupleOrCombination):
+# 	if len(tupleOrCombination) == length:
+# 		allPossibility.append(tupleOrCombination)
+# 		return
+# 
+# 	if isTupleOrCombination:
+# 		# tuple
+# 		elementLowerBound = 0
+# 		elementUpperBound = tupleElementUpperBound
+# 	else:
+# 		# combination
+# 		if len(tupleOrCombination) == 0:
+# 			elementLowerBound = 0
+# 		else:
+# 			elementLowerBound = tupleOrCombination[len(tupleOrCombination) - 1] + 1
+# 		elementUpperBound = tupleElementUpperBound - (length - len(tupleOrCombination)) + 1
+# 		
+# 	for i in range(elementLowerBound, elementUpperBound):
+# 		generateAllTupleOrCombinationRecursion(allPossibility, length, tupleElementUpperBound, isTupleOrCombination, tupleOrCombination + (i,))
 
 def generateAllTupleOrCombination(length, elementUpperBound, isTupleOrCombination):
-	allPossibility = []
-	generateAllTupleOrCombinationRecursion(allPossibility, length, elementUpperBound, isTupleOrCombination, tuple())
-	return allPossibility
+	if isTupleOrCombination == TUPLE:
+		return list(itertools.product(range(elementUpperBound), repeat=length))
+	else:
+		return list(itertools.combinations(range(elementUpperBound), length))
+	# allPossibility = []
+	# generateAllTupleOrCombinationRecursion(allPossibility, length, elementUpperBound, isTupleOrCombination, tuple())
+	# return allPossibility
 
 NEGATIVE_INFINITY = float('-inf')
 
